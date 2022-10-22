5c19840823d4d7b92f92bf197859de9af06e84fa
Arnab <iamarnabbiswas2001@gmail.com>
2022-10-22 15:55:52 +0530

    Mt first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// traversing the dom
+// const btns=document.querySelectorAll('.question-btn')
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//        const question= e.currentTarget.parentElement.parentElement;
+//       question.classList.toggle('show-text')
+//     })
+// })
+// using selectors inside the element
+const questions=document.querySelectorAll('.question')
+questions.forEach(function(question){
+    const btn=question.querySelector('.question-btn')
+    btn.addEventListener('click',()=>{
+        questions.forEach(function(item){
+           if(item!==question)
+           item.classList.remove('show-text')
+        })
+        question.classList.toggle('show-text')
+    })
+})
